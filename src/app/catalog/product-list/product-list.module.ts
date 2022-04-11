3307509697f2d08c062f8ac467380f2f066d0016
dnovysh <94901721+dnovysh@users.noreply.github.com>
2022-04-11 04:02:36 +0300

    Add css to product list

--- src/app/catalog/product-list/product-list.module.ts
+++ src/app/catalog/product-list/product-list.module.ts
@@ -3,6 +3,13 @@ import {StoreModule} from "@ngrx/store";
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MessageService} from "primeng/api";
+import {DataViewModule} from "primeng/dataview";
+import {ButtonModule} from "primeng/button";
+import {ToastModule} from "primeng/toast";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {PanelModule} from "primeng/panel";
+import {FormsModule} from "@angular/forms";
+import {RippleModule} from "primeng/ripple";
 
 import {ProductListComponent} from './components/product-list/product-list.component';
 import {ProductListService} from "src/app/catalog/product-list/services/product-list.service";
@@ -10,25 +17,26 @@ import {reducer} from "src/app/catalog/product-list/store/reducers";
 import {GetProductListEffect} from "src/app/catalog/product-list/store/effects/get-product-list.effect";
 import {LoadingModule} from "src/app/shared/modules/loading/loading.module";
 import {ErrorMessageModule} from "src/app/shared/modules/error-message/error-message.module";
-import {DataViewModule} from "primeng/dataview";
-import {ButtonModule} from "primeng/button";
-import {ToastModule} from "primeng/toast";
 
 
 @NgModule({
   declarations: [
     ProductListComponent
   ],
-    imports: [
-        CommonModule,
-        EffectsModule.forFeature([GetProductListEffect]),
-        StoreModule.forFeature('productList', reducer),
-        LoadingModule,
-        ErrorMessageModule,
-        DataViewModule,
-        ButtonModule,
-        ToastModule
-    ],
+  imports: [
+    CommonModule,
+    DataViewModule,
+    ButtonModule,
+    SelectButtonModule,
+    PanelModule,
+    RippleModule,
+    FormsModule,
+    ToastModule,
+    EffectsModule.forFeature([GetProductListEffect]),
+    StoreModule.forFeature('productList', reducer),
+    LoadingModule,
+    ErrorMessageModule
+  ],
   providers: [
     ProductListService,
     MessageService
