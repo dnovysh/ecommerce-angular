7d896390c568c5c872a40ba351a6c6c3f8b92505
dnovysh <94901721+dnovysh@users.noreply.github.com>
2022-04-16 04:16:04 +0300

    Add sample images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,41 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {StoreModule} from "@ngrx/store";
-import {EffectsModule} from "@ngrx/effects";
-import {routerReducer, StoreRouterConnectingModule} from "@ngrx/router-store";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { routerReducer, StoreRouterConnectingModule } from "@ngrx/router-store";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ProductListModule} from "src/app/catalog/product-list/product-list.module";
-import {LoadingModule} from "src/app/shared/modules/loading/loading.module";
-import {environment} from "src/environments/environment";
-import {SharedModule} from "primeng/api";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListModule } from "src/app/catalog/product-list/product-list.module";
+import { LoadingModule } from "src/app/shared/modules/loading/loading.module";
+import { environment } from "src/environments/environment";
+import { SharedModule } from "primeng/api";
+import { AppFooterModule } from "src/app/shared/modules/app-footer/app-footer.module";
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        // FormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        StoreModule.forRoot({router: routerReducer}, {}),
-        EffectsModule.forRoot([]),
-        StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
-        StoreRouterConnectingModule.forRoot(),
-        ProductListModule,
-        LoadingModule,
-        SharedModule
-    ],
+  imports: [
+    BrowserModule,
+    // FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({ router: routerReducer }, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
+    ProductListModule,
+    LoadingModule,
+    SharedModule,
+    AppFooterModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/catalog/product-list/product-list.module.ts
+++ src/app/catalog/product-list/product-list.module.ts
@@ -1,25 +1,34 @@
-import {EffectsModule} from "@ngrx/effects";
-import {StoreModule} from "@ngrx/store";
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MessageService} from "primeng/api";
-import {DataViewModule} from "primeng/dataview";
-import {ButtonModule} from "primeng/button";
-import {ToastModule} from "primeng/toast";
-import {SelectButtonModule} from "primeng/selectbutton";
-import {PanelModule} from "primeng/panel";
-import {FormsModule} from "@angular/forms";
-import {RippleModule} from "primeng/ripple";
-import {RatingModule} from "primeng/rating";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageService } from "primeng/api";
+import { DataViewModule } from "primeng/dataview";
+import { ButtonModule } from "primeng/button";
+import { ToastModule } from "primeng/toast";
+import { SelectButtonModule } from "primeng/selectbutton";
+import { PanelModule } from "primeng/panel";
+import { FormsModule } from "@angular/forms";
+import { RippleModule } from "primeng/ripple";
+import { RatingModule } from "primeng/rating";
+import { RouterModule } from "@angular/router";
 
-import {ProductListComponent} from './components/product-list/product-list.component';
-import {ProductListService} from "src/app/catalog/product-list/services/product-list.service";
-import {reducer} from "src/app/catalog/product-list/store/reducers";
-import {GetProductListEffect} from "src/app/catalog/product-list/store/effects/get-product-list.effect";
-import {LoadingModule} from "src/app/shared/modules/loading/loading.module";
-import {ErrorMessageModule} from "src/app/shared/modules/error-message/error-message.module";
-import {ImagePreloadModule} from "src/app/shared/directives/image-preload/image-preload.module";
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductListService } from "src/app/catalog/product-list/services/product-list.service";
+import { reducer } from "src/app/catalog/product-list/store/reducers";
+import { GetProductListEffect } from "src/app/catalog/product-list/store/effects/get-product-list.effect";
+import { LoadingModule } from "src/app/shared/modules/loading/loading.module";
+import { ErrorMessageModule } from "src/app/shared/modules/error-message/error-message.module";
+import { ImagePreloadModule } from "src/app/shared/directives/image-preload/image-preload.module";
+import { ScrollPanelModule } from "primeng/scrollpanel";
+import { DividerModule } from "primeng/divider";
 
+
+const routes = [
+  { path: 'category/:categoryId', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent }
+]
 
 @NgModule({
   declarations: [
@@ -34,12 +43,15 @@ import {ImagePreloadModule} from "src/app/shared/directives/image-preload/image-
     RippleModule,
     FormsModule,
     ToastModule,
+    RouterModule.forChild(routes),
     EffectsModule.forFeature([GetProductListEffect]),
     StoreModule.forFeature('productList', reducer),
     LoadingModule,
     ErrorMessageModule,
     RatingModule,
-    ImagePreloadModule
+    ImagePreloadModule,
+    ScrollPanelModule,
+    DividerModule
   ],
   providers: [
     ProductListService,
@@ -49,5 +61,4 @@ import {ImagePreloadModule} from "src/app/shared/directives/image-preload/image-
     ProductListComponent
   ]
 })
-export class ProductListModule {
-}
+export class ProductListModule {}
