99945d287f10073c0f69c24fdfe3fa97b9708be6
dnovysh <94901721+dnovysh@users.noreply.github.com>
2022-04-20 03:09:42 +0300

    Navbar styling

---
+++ src/app/catalog/category-menu/category-menu.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryMenuComponent } from './components/category-menu/category-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoryMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryMenuModule { }
